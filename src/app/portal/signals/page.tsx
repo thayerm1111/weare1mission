35477ad85d9330124631424bbc1e180d08e6bb2f
thayerm1111 <thayerm1111@gmail.com>
2026-07-25 16:42:03 -0500

    Add files via upload

---
+++ src/app/portal/signals/page.tsx
@@ -0,0 +1,7 @@
+import { SignalGenerator } from "@/components/portal/SignalGenerator";
+
+export const metadata = { title: "Signal Hub" };
+
+export default function SignalsPage() {
+  return <SignalGenerator />;
+}
