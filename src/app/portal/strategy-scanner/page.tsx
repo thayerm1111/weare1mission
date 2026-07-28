ec015868d37b39c553150fe6057a6cb7d1c2b927
thayerm1111 <thayerm1111@gmail.com>
2026-07-27 23:22:31 -0500

    Create page.tsx

---
+++ src/app/portal/strategy-scanner/page.tsx
@@ -0,0 +1,8 @@
+import { StrategyScanner } from "@/components/portal/StrategyScanner";
+
+export const metadata = { title: "OM Strategy Scanner" };
+export const dynamic = "force-dynamic";
+
+export default function StrategyScannerPage() {
+  return <StrategyScanner />;
+}
