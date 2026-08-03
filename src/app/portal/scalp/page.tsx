3b08a08ca5cb7ffdf2cf083c5f45fbb027cc8ae6
thayerm1111 <thayerm1111@gmail.com>
2026-08-02 23:15:27 -0500

    Add files via upload

---
+++ src/app/portal/scalp/page.tsx
@@ -0,0 +1,8 @@
+import { ScalpTool } from "@/components/portal/ScalpTool";
+
+export const metadata = { title: "OM Scalp" };
+export const dynamic = "force-dynamic";
+
+export default function ScalpPage() {
+  return <ScalpTool />;
+}
