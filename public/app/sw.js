bb1665f6a088f49eab5885b77231c4077de90476
thayerm1111 <thayerm1111@gmail.com>
2026-08-21 11:06:00 -0500

    Add files via upload

--- public/app/sw.js
+++ public/app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "om-app-v8";
+const CACHE = "om-app-v9";
 const SHELL = ["/app/index.html", "/app/manifest.webmanifest", "/app/icon-192.png", "/app/icon-512.png"];
 self.addEventListener("install", (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL).catch(() => {})).then(() => self.skipWaiting())); });
 self.addEventListener("activate", (e) => { e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())); });
